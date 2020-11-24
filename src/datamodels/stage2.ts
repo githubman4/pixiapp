4795561f61098afd2e9561bfeb878320190390b1
macbookpro2009mid <osman419218@gmail.com>
2020-11-24 20:29:54 +0900

    aaa

--- src/datamodels/stage2.ts
+++ src/datamodels/stage2.ts
@@ -13,13 +13,12 @@ export const CreateStage2 = (model: Model, stageType: STAGE_TYPE,
   const stg = new ModelStage(stageType, PixiContainer, status, (delta: number) => {
     // update animation
 
-    PixiContainer.children.forEach((e) => {
+    /* PixiContainer.children.forEach((e) => {
       if (e.name === 'osman') {
         e.rotation -= 0.05 * delta
-      } else {
-        e.rotation += 0.05 * delta
       }
     })
+    */
 
     // 新しいオブジェのUPDATE
     sp_objects.forEach((e:SpriteObj) => {
@@ -42,47 +41,52 @@ export const CreateStage2 = (model: Model, stageType: STAGE_TYPE,
 function fnInitialize (model: Model, stg: ModelStage, sp_objects: SpriteObj[]) {
   // 前のステージに戻る
   const tx1 = path.resolve('./images', './ArrowLeft.gif')
-  const sp1 = new PIXI.Sprite(PIXI.Texture.from(tx1))
-  sp1.anchor.x = 0
-  sp1.anchor.y = 0
-  sp1.interactive = true
-  sp1.buttonMode = true
-  sp1.position.set(0, 0)
-  sp1.on('pointertap', () => {
+  // const sp1 = new PIXI.Sprite(PIXI.Texture.from(tx1))
+  const sp1 = new SpriteObj('arrow', tx1, stg, () => { })
+  sp1.sp.anchor.x = 0
+  sp1.sp.anchor.y = 0
+  sp1.sp.interactive = true
+  sp1.sp.buttonMode = true
+  sp1.sp.position.set(0, 0)
+  sp1.sp.on('pointertap', () => {
     model.changeStage(STAGE_TYPE.PLAY)
   })
-  stg.container.addChild(sp1)
+  // stg.container.addChild(sp1)
+  sp_objects.push(sp1)
 
   // ******************************************************
   const tx2 = path.resolve('./images', './human_dot_color.gif')
-  const sp2 = new PIXI.Sprite(PIXI.Texture.from(tx2))
-  sp2.anchor = new PIXI.Point(0.5, 0.5)
-  sp2.interactive = true
-  sp2.buttonMode = true
-  sp2.name = 'osman'
-  sp2.position.set(50, 50)
-  sp2.on('pointertap', () => {
+  const sp2 = new SpriteObj('osman', tx2, stg,
+    (obj: SpriteObj, delta: number) => {
+      obj.sp.rotation += 0.05 * delta
+    })
+  sp2.sp.anchor = new PIXI.Point(0.5, 0.5)
+  sp2.sp.interactive = true
+  sp2.sp.buttonMode = true
+  sp2.sp.position.set(50, 50)
+  sp2.sp.on('pointertap', () => {
     alert(sp2.name)
   })
-  stg.container.addChild(sp2)
+  sp_objects.push(sp2)
   // ******************************************************
-  // const tx2 = path.resolve('./images', './human_dot_color.gif')
-  const sp3 = new PIXI.Sprite(PIXI.Texture.from(tx2))
-  sp3.anchor = new PIXI.Point(0.5, 0.5)
-  sp3.interactive = true
-  sp3.buttonMode = true
-  sp3.name = 'puman'
-  sp3.position.set(150, 150)
-  sp3.on('pointertap', () => {
+  const sp3 = new SpriteObj('pusman', tx2, stg, (obj: SpriteObj, delta: number) => {
+    obj.sp.rotation += 0.05 * delta
+  })
+  sp3.sp.anchor = new PIXI.Point(0.5, 0.5)
+  sp3.sp.interactive = true
+  sp3.sp.buttonMode = true
+  sp3.sp.position.set(150, 150)
+  sp3.sp.on('pointertap', () => {
     alert(sp3.name)
   })
-  stg.container.addChild(sp3)
+  sp_objects.push(sp3)
+
   // ******************************************************
 
   // 新しいスプライトオベジェクトの追加
 
   const sp4 = new SpriteObj('NEW Osman', tx2, stg, (obj:SpriteObj, delta: number) => {
-    obj.sp.rotation += 0.01 * delta
+    obj.sp.rotation -= 0.01 * delta
   })
   sp4.sp.position.set(80, 80)
   sp4.sp.on('pointertap', () => {
